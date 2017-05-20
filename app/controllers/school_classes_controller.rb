c8ae4c73b49da7aa8ca8cd559c81c5c191bd9ea5
Robert Wayne <robertjwayne@gmail.com>
2017-05-19 23:11:36 -0400

    Add create action

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -3,6 +3,11 @@ class SchoolClassesController < ApplicationController
     @school_class = SchoolClass.new
   end
 
+  def create
+    @school_class = SchoolClass.create(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
+  end
+
   private
 
   def school_class_params(*args)
