4181666d9a90d0f885a5751545835f458a91b9ef
Robert Wayne <robertjwayne@gmail.com>
2017-05-19 23:40:27 -0400

    Add update action to controllers for school classes and students

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -16,6 +16,12 @@ class SchoolClassesController < ApplicationController
     @school_class = SchoolClass.find(params[:id])
   end
 
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
+  end
+
   private
 
   def school_class_params(*args)
