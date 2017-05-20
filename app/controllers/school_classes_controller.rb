411f0bcdb778d9d26d5b4205d4af44c767e749a7
Robert Wayne <robertjwayne@gmail.com>
2017-05-19 23:32:16 -0400

    Add edit action

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,11 +1,15 @@
 class SchoolClassesController < ApplicationController
-  def new
-    @school_class = SchoolClass.new
-  end
-
   def create
     @school_class = SchoolClass.create(school_class_params(:title, :room_number))
     redirect_to school_class_path(@school_class)
+  end
+
+  def edit
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def new
+    @school_class = SchoolClass.new
   end
 
   def show
