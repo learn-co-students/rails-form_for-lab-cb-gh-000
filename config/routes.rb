f7f21f676f5d946ff179b8a78c1a6dc512c6ab42
Robert Wayne <robertjwayne@gmail.com>
2017-05-19 22:01:36 -0400

    Add create, edit, index, new, show, and update routes for school_classes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :students, only: [:create, :edit, :index, :new, :show, :update]
+  resources :school_classes, only: [:create, :edit, :index, :new, :show, :update]
 end
