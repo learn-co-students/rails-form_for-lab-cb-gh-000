c2ea414e9e5a0bfcaded78039860f4320a2c5525
Robert Wayne <robertjwayne@gmail.com>
2017-05-19 23:12:15 -0400

    Add create action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,11 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+    @student = Student.create(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
   def index
     @students = Student.all
   end
