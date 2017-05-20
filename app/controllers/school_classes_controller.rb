9e2fabfe65ba5c7162338f096aa94347ae0c92ea
Robert Wayne <robertjwayne@gmail.com>
2017-05-19 23:00:04 -0400

    Add school_class_params method

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -2,4 +2,10 @@ class SchoolClassesController < ApplicationController
   def new
     @school_class = SchoolClass.new
   end
+
+  private
+
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
+  end
 end
