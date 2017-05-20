740fc3bdbc3c92c1bbedc0df64102db0827735c4
Robert Wayne <robertjwayne@gmail.com>
2017-05-19 22:16:36 -0400

    Add student_params method

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,10 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  private
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
+  end
 end
