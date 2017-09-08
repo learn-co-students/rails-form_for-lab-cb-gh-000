9b9bb3216696e501007f96fcf7c919d138d3c476
Saujan Ghimire <supertrampsookah@gmail.com>
2017-09-08 10:22:49 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :school_classes
+  resources :students
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
@@ -15,7 +16,7 @@ Rails.application.routes.draw do
   #   resources :products
 
   # Example resource route with options:
-  #   resources :products do
+  #   resources :productrs do
   #     member do
   #       get 'short'
   #       post 'toggle'
